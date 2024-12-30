ba2f367fcaf5f90311d0f6115340f9404d57220a
Paweł Pietras <pawel.pietras144@gmail.com>
2024-12-30 19:25:12 +0100

    Stworzenie switcha do zmiany języków #5

--- components/LangSwitcher.vue
+++ components/LangSwitcher.vue
@@ -1,4 +1,7 @@
 <script setup lang="ts">
+import { computed, ref } from "vue";
+import { useI18n } from "vue-i18n";
+
 const { t, locales, locale, setLocale } = useI18n();
 
 const language = computed({
@@ -7,18 +10,30 @@ const language = computed({
     setLocale(value);
   },
 });
+
+const languageOptions = computed(() => {
+  return locales.value.map((item) => ({
+    label: t(`language_names.${item.code}`),
+    value: item.code,
+    icon: item.icon,
+  }));
+});
+
+const selected = computed({
+  get: () =>
+    languageOptions.value.find((option) => option.value === language.value),
+  set: (newValue) => {
+    if (newValue && newValue.value) {
+      language.value = newValue.value;
+    }
+  },
+});
 </script>
+
 <template>
-  <ClientOnly>
-    <select v-model="language">
-      <option
-        v-for="item in locales"
-        :key="item.code"
-        :value="item.code"
-        class="flex items-center"
-      >
-        {{ t(`language_names.${item.code}`) }}
-      </option>
-    </select>
-  </ClientOnly>
+  <USelectMenu v-model="selected" :options="languageOptions">
+    <template #leading>
+      <UIcon v-if="selected?.icon" :name="selected?.icon" class="w-5 h-5" />
+    </template>
+  </USelectMenu>
 </template>
