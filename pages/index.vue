2214fd6e744362deb91a7dcdccb4efab9751b409
Paweł Pietras <pawel.pietras144@gmail.com>
2025-01-03 23:37:40 +0100

    Animacja zmiany tekstu (co robimy jako stolarnia) #17

--- pages/index.vue
+++ pages/index.vue
@@ -98,26 +98,26 @@ onUnmounted(() => {
         </template>
         <template #links>
           <div class="mt-10 flex flex-wrap justify-center gap-x-6 gap-y-3">
-            <a
+            <NuxtLink
+              to="/portfolio"
               class="focus:outline-none focus-visible:outline-0 disabled:cursor-not-allowed disabled:opacity-75 aria-disabled:cursor-not-allowed aria-disabled:opacity-75 flex-shrink-0 transition-all font-medium rounded-md text-sm gap-x-2.5 px-3.5 py-2.5 shadow-sm ring-1 ring-inset ring-gray-300 dark:ring-gray-700 text-gray-700 dark:text-gray-200 bg-gray-50 hover:bg-gray-100 disabled:bg-gray-50 aria-disabled:bg-gray-50 dark:bg-gray-800 dark:hover:bg-gray-950 dark:disabled:bg-gray-800 dark:aria-disabled:bg-gray-800 focus-visible:ring-2 focus-visible:ring-primary-500 dark:focus-visible:ring-primary-400 inline-flex items-center"
-              href="/portfolio"
             >
               <UIcon
                 name="i-heroicons-check-badge-16-solid"
                 class="flex-shrink-0 h-5 w-5"
               />
-              <span class="">Zobacz realizacje</span></a
-            >
-            <a
+              <span>Zobacz realizacje</span>
+            </NuxtLink>
+            <NuxtLink
+              to="/contact"
               class="focus:outline-none focus-visible:outline-0 disabled:cursor-not-allowed disabled:opacity-75 aria-disabled:cursor-not-allowed aria-disabled:opacity-75 flex-shrink-0 transition-all font-medium rounded-md text-sm gap-x-2.5 px-3.5 py-2.5 shadow-sm ring-1 ring-inset ring-gray-300 dark:ring-gray-700 text-gray-700 dark:text-gray-200 bg-gray-50 hover:bg-gray-100 disabled:bg-gray-50 aria-disabled:bg-gray-50 dark:bg-gray-800 dark:hover:bg-gray-950 dark:disabled:bg-gray-800 dark:aria-disabled:bg-gray-800 focus-visible:ring-2 focus-visible:ring-primary-500 dark:focus-visible:ring-primary-400 inline-flex items-center"
-              href="/contact"
             >
               <UIcon
                 name="i-heroicons-phone-16-solid"
                 class="flex-shrink-0 h-5 w-5"
               />
-              <span class="">Kontakt</span>
-            </a>
+              <span>Kontakt</span>
+            </NuxtLink>
           </div>
         </template>
       </ULandingHero>
---
+++ pages/partners.vue
@@ -0,0 +1,1 @@
+<template>Partners</template>
